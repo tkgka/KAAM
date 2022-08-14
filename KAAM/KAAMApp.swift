988a90a3befd6e75df9847d7e9b3942775e79be0
tkgka <ksh17171@gmail.com>
2022-08-14 18:32:43 +0900

    Initial Commit

---
+++ KAAM/KAAMApp.swift
@@ -0,0 +1,17 @@
+//
+//  KAAMApp.swift
+//  KAAM
+//
+//  Created by 김수환 on 2022/08/14.
+//
+
+import SwiftUI
+
+@main
+struct KAAMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
